af8ea3d0f067645f1b46d0e7d770a57415135f4f
Shumail-AbdulRehman <47891@students.riphah.edu.pk>
2025-10-03 09:37:16 +0500

    added crousla for product

--- src/components/customs/FeaturedProducts.jsx
+++ src/components/customs/FeaturedProducts.jsx
@@ -3,6 +3,13 @@ import userService from "../../appwrite/services"
 import ProductCard from "./ProductCard"
 import LoadingSpinner from "./LoadingSpinner"
 import { Link } from "react-router-dom"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 
 function FeaturedProducts() {
   const [loading, setLoading] = useState(false)
@@ -21,7 +28,6 @@ function FeaturedProducts() {
         setLoading(false)
       }
     }
-
     fetchFeaturedProducts()
   }, [])
 
@@ -30,9 +36,9 @@ function FeaturedProducts() {
   }
 
   return (
-    <section className="w-full py-10 px-6 md:px-12 lg:px-20 ">
+    <section className="w-full py-10 px-6 md:px-12 lg:px-20">
       <div className="max-w-7xl mx-auto text-center">
-        <h2 className="text-3xl md:text-5xl font-extrabold  mt-3 mb-6 text-black">
+        <h2 className="text-3xl md:text-5xl font-extrabold mt-3 mb-6 text-black">
           🔥 Featured Products
         </h2>
         <p className="text-gray-600 mb-12 max-w-2xl mx-auto">
@@ -43,12 +49,29 @@ function FeaturedProducts() {
         {featuredProducts.length === 0 ? (
           <p className="text-gray-500">No featured products available</p>
         ) : (
-          <div className="flex flex-wrap justify-center gap-10">
-            {featuredProducts.map((product) => (
-              <Link to={`/categories/${product.$id}`} key={product.$id} className="flex-shrink-0">
-                <ProductCard product={product} />
-              </Link>
-            ))}
+          <div className="relative px-12">
+            <Carousel
+              opts={{
+                align: "center",
+                loop: false,
+              }}
+              className="w-full"
+            >
+              <CarouselContent className="flex items-center">
+                {featuredProducts.map((product) => (
+                  <CarouselItem
+                    key={product.$id}
+                    className=" basis-1/1 sm:basis-1/2 md:basis-1/2 lg:basis-1/4 flex justify-center"
+                  >
+                    <Link to={`/categories/${product.$id}`} className="w-full max-w-sm flex justify-center">
+                      <ProductCard product={product} />
+                    </Link>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious className="absolute left-0" />
+              <CarouselNext className="absolute right-0" />
+            </Carousel>
           </div>
         )}
       </div>
